18e9107d1f6dba6bed2a154341c3c8e9a66dc363
Ronak Makwana <ronakthedeveloper@gmail.com>
2026-06-12 16:21:22 +0530

    update

--- test.py
+++ test.py
@@ -1,71 +1,49 @@
-def word_counter(sentence):
-    if sentence == '':
-        return  {}
-    words = sentence.split(' ')
+import csv
 
-    words = map(lambda w : w.lower(), words)
+def read_csv(path):
+    with open(path) as file:
+        reader = csv.DictReader(file)
+        rows = []
+        for r in reader:
+            rows.append(r)
 
-    frequency_dict: dict[str, int] = {}
+        return rows
 
-    for word in words:
-        if word in frequency_dict:
-            frequency_dict[word] += 1
-        else:
-            frequency_dict[word] = 1
+def write_csv(path, record):
 
-    return frequency_dict
+    if len(record) == 0:
+        return
 
-def run_tests():
-    test_cases = [
-        (
-            "hello world hello python",
-            {"hello": 2, "python": 1, "world": 1}
-        ),
-        (
-            "Python python PYTHON Java java",
-            {"java": 2, "python": 3}
-        ),
-        (
-            "coding",
-            {"coding": 1}
-        ),
-        (
-            "cat dog cat bird dog cat",
-            {"bird": 1, "cat": 3, "dog": 2}
-        ),
-        (
-            "apple banana cherry apple banana",
-            {"apple": 2, "banana": 2, "cherry": 1}
-        ),
-        (
-            "zebra apple mango zebra apple",
-            {"apple": 2, "mango": 1, "zebra": 2}
-        ),
-        (
-            "",
-            {}
-        ),
-        (
-            "1 2 1 python 2 python",
-            {"1": 2, "2": 2, "python": 2}
-        )
-    ]
-
-    passed = 0
-
-    for i, (sentence, expected) in enumerate(test_cases, 1):
-        result = word_counter(sentence)
-
-        if result == expected:
-            print(f"Test {i}: PASSED ✅")
-            passed += 1
-        else:
-            print(f"Test {i}: FAILED ❌")
-            print("Input:   ", sentence)
-            print("Expected:", expected)
-            print("Got:     ", result)
-
-    print(f"\nScore: {passed}/{len(test_cases)}")
+    with open(path, mode='w') as file:
+        col = list(record[0])
+        writer = csv.DictWriter(file, fieldnames=col)
+        writer.writeheader()
+        writer.writerows(record)        
 
 
-run_tests()
+rows = read_csv('phase1/employees.csv')
+
+correct_rows = []
+incorrect_rows = []
+
+for row in rows:
+    try:
+        correct_rows.append({
+            "name" : row['name'],
+            "salary" : int(row['salary'] if row['salary'] else None)
+        })
+    except ValueError:
+        incorrect_rows.append({
+            "name" : row['name'],
+            "msg" : 'Invalida Data Type'
+        });
+    except TypeError:
+        incorrect_rows.append({
+            "name" : row['name'],
+            "msg" : 'Missing Value'
+        });
+
+print(correct_rows)
+print(incorrect_rows)
+write_csv('phase1/employees_correct_rows.csv', correct_rows)
+write_csv('phase1/employees_incorrect_rows.csv', incorrect_rows)
