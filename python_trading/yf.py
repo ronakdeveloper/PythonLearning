87938792357592da63034a895abad54f1e2f9783
Ronak Makwana <ronakthedeveloper@gmail.com>
2026-07-03 17:29:03 +0530

    update

--- python_trading/yf.py
+++ python_trading/yf.py
@@ -39,13 +39,6 @@ def generate_signal(data,size1,size2):
     data['entry_price'] = data['open'].shift(-1)
     return data
 
-
-result = get_data('ADANIPOWER.NS','2026-01-01','2026-07-02','1D')
-
-result = add_sma(data=result,size=5)
-result = add_sma(data=result,size=20)
-result = generate_signal(data=result,size1=5,size2=20)
-
 def build_trade_log(result):    
     buy_rows = result[result['signal']]
     exit_rows = result[result['exit_signal']]
@@ -68,14 +61,30 @@ def build_trade_log(result):
     data = pd.DataFrame(trades)
     return data
 
+
+def calculate_metrics(trade_log):
+    total_return = trade_log['percentage'].sum()
+    win_rate = (trade_log['percentage'] > 0).mean() * 100
+    avg_win = trade_log['percentage'][trade_log['percentage'] > 0].mean()
+    avg_loss = trade_log['percentage'][trade_log['percentage'] < 0].mean()
+    profit_factor = (trade_log['percentage'][trade_log['percentage'] > 0].sum()) / abs(trade_log['percentage'][trade_log['percentage'] < 0].sum())
+    
+    return {
+        'total_return' : round(total_return,2),
+        'win_rate' : round(win_rate,2),
+        'avg_win' : avg_win,
+        'avg_loss' : avg_loss,
+        'profit_factor' : profit_factor
+    }
+
+
+result = get_data('BEL.NS','2025-01-01','2026-07-02','1D')
+
+
+result = add_sma(data=result,size=13)
+result = add_sma(data=result,size=48)
+result = generate_signal(data=result,size1=13,size2=48)
 trade_log = build_trade_log(result)
-print(trade_log)
 
-# def calculate_metrics():
-
-total_return = trade_log['percentage'].sum()
-win_rate = (trade_log['percentage'] > 0).mean() * 100
-avg_win = trade_log['percentage'][trade_log['percentage'] > 0].mean()
-avg_loss = trade_log['percentage'][trade_log['percentage'] < 0].mean()
-profit_factor = (trade_log['percentage'][trade_log['percentage'] > 0].sum()) / abs(trade_log['percentage'][trade_log['percentage'] < 0].sum())
-print(profit_factor)
+output = calculate_metrics(trade_log)
+print(output)
