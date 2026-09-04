444779e481e9eb202676223fe470da21ef9ce5f2
Ronak Makwana <ronakthedeveloper@gmail.com>
2026-09-04 16:42:47 +0530

    add

---
+++ OOPs/ex2.py
@@ -0,0 +1,25 @@
+class Shape:
+    def area(self):
+        return 0
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+
+arr = [Rectangle(5,10), Circle(5)]
+
+for i in arr:
+    print(i.area())
+
